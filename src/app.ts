2a300db8bb25a31d0195ef3b465b9db9c3290d55
baltasarromero <baltasarromero@gmail.com>
2023-03-22 13:09:44 -0300

    getListingCreatedEvent

--- src/app.ts
+++ src/app.ts
@@ -3,37 +3,39 @@ dotenv.config();
 
 const { ethers } = require("ethers");
 import { InfuraProvider } from "@ethersproject/providers";
-import { Contract } from "@ethersproject/contracts";
+import { Contract, Event, EventFilter } from "@ethersproject/contracts";
 const NFTMarketplaceArtifact = require("../artifacts/NFTMarketplace.sol/NFTMarketplace.json");
 
 
 let nftContract: Contract;
 let provider: InfuraProvider;
 
-async function setProvider() {
+function setup() {
+    // Configure provider
     provider = new ethers.providers.InfuraProvider(
         process.env.ETHEREUM_NETWORK,
         process.env.INFURA_PROJECT_ID
     );
+
+    // Configure contract
+    nftContract = new ethers.Contract(process.env.NFT_MARKETPLACE_CONTRACT_ADDRESS, NFTMarketplaceArtifact.abi, provider);
 }
 
-async function getNFTMarketplaceEvents() {
-    await setProvider();
-
-    nftContract = new ethers.Contract("0x0681F44AADF8FA1eA9aC001cF0AB688266c68985", NFTMarketplaceArtifact.abi, provider);
-
+async function getCreatedListingEvents(): Promise<Array<Event>> {
     console.log(`Getting the nft marketplace events events...`);
     const currentBlock = await provider.getBlockNumber();
 
-    let listingCreatedEventFilter = nftContract.filters.ListingCreated();
-    let events = await nftContract.queryFilter(listingCreatedEventFilter, 0, currentBlock);
+    let listingCreatedEventFilter: EventFilter = nftContract.filters.ListingCreated();
+    let events: Array<Event>;
+    events = await nftContract.queryFilter(listingCreatedEventFilter, 0, currentBlock);
   
-    console.log(events);
+    return events;
 }
 
 
 async function main () {
-    await getNFTMarketplaceEvents();
+    setup();
+    console.log(await getCreatedListingEvents());
 }
 
 main();
